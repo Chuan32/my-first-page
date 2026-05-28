c9de610b0cd70025d9c56b982ebc3718bd23b464
川 <3378526603@qq.com>
2026-05-28 10:58:13 +0800

    第18次课，弄得、

--- server.js
+++ server.js
@@ -10,13 +10,16 @@ let server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         res.end("<h1>首页</h1><p>这是我第一个Node.js服务器</p>");
     }
-    else if(req.url === "/data" || req.url === "/hobby"){
+    else if(req.url === "/data"){
         res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
         let data = {name: "川", age: 20, goal: "全栈工程师"};
-        let hobby = ["打游戏", "打羽毛球", "跑步"];
         res.end(JSON.stringify(data));
+    } else if(req.url === "/hobby"){ 
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        let hobby = ["打游戏", "打羽毛球", "跑步"];
         res.end(JSON.stringify(hobby));
-    } else{
+    }
+     else{
         res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
         res.end("<h1>404</h1><p>找不到该页面</p>");
     }
